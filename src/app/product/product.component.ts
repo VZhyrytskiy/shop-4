edabc851519bc959120ff78a03a42f402560b9df
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-11 20:17:04 +0300

    refactor: delete public modificator

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -15,12 +15,12 @@ enum Category {
   styleUrls: ['./product.component.less'],
 })
 export class ProductComponent implements OnInit {
-  public name: string;
-  public description: string;
-  public price: number;
-  public category: Category;
-  public isAvailable: boolean;
-  public authors: string[];
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+  authors: string[];
 
   constructor() { }
 
