93351fb16a0f765d8533b8c99271ff264f292799
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-11 20:40:59 +0300

    feature: change folder structure and add products service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [AppComponent, ProductComponent],
