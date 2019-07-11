7c6c9e1b27c72b38b9b3d652a5a693ced0e1239a
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-11 20:08:59 +0300

    feature: add product properties

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Category {
+  Mathematics,
+  Physics,
+  Biology,
+  Literature,
+  Chemistry,
+  Programming,
+}
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.less'],
 })
 export class ProductComponent implements OnInit {
+  public name: string;
+  public description: string;
+  public price: number;
+  public category: Category;
+  public isAvailable: boolean;
+  public authors: string[];
 
   constructor() { }
 
   ngOnInit() {
+	  this.name = 'ng-book';
+	  this.description = 'The Complete Book on Angular';
+	  this.price = 79;
+	  this.category = Category.Programming;
+	  this.isAvailable = true;
+	  this.authors = ['Nate Murray', 'Felipe Coury', 'Ari Lerner', 'Carlos Taborda'];
   }
 
 }
