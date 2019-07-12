52085ab114a8f51766e8899a4a304b8f5d8fb47d
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-12 09:16:48 +0300

    feature: add cart component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductsService } from './components/product/products.service';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, ProductListComponent],
+  declarations: [AppComponent, ProductComponent, ProductListComponent, CartComponent],
 	imports: [BrowserModule, AppRoutingModule],
   providers: [ProductsService],
   bootstrap: [AppComponent],
