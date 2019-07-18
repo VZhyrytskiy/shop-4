23bb6b995a6d6c9f5ce2619392205734a60a8ba5
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-18 17:05:02 +0300

    feature: add total amount and sum to cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,8 +10,10 @@ import { IProduct } from '../product/product.model';
 export class CartComponent implements OnInit {
 	constructor(private cartService: CartService) {}
 	productsInBasket: IProduct[];
+	totalSum: number;
 
 	ngOnInit() {
 		this.productsInBasket = this.cartService.getProductsInBasket();
+		this.totalSum = this.productsInBasket.reduce((sum, current) => sum + current.price, 0);
 	}
 }
