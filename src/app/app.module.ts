989c3010a3190a916d80bf1b27146c1992adf978
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-18 12:32:25 +0300

    feature: replace components to products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { CartComponent } from './components/cart/cart.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './components/products.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		ProductComponent,
-		ProductListComponent,
-		CartComponent,
 	],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		CoreModule,
+		SharedModule,
+		ProductsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ src/app/components/products.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+
+@NgModule({
+	declarations: [
+		ProductComponent,
+		ProductListComponent,
+		CartComponent,
+	],
+	imports: [
+		CommonModule,
+	],
+	exports: [
+		ProductComponent,
+		ProductListComponent,
+		CartComponent,
+	],
+})
+export class ProductsModule {}
