47d56e623008067bb192abbc52f4c3cfa373b823
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-19 14:16:02 +0300

    feature: add appTitle variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-	title = 'shop';
+export class AppComponent implements AfterViewInit {
+	@ViewChild('appTitle', { static: false })
+	title: ElementRef<HTMLElement>;
+
+	ngAfterViewInit() {
+		this.title.nativeElement.innerHTML = 'Welcome to the Book Shop!!!';
+	}
 }
