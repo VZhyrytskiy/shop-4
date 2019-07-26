295127ef304a2325ca79119c19947cef996d1724
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-07-26 12:01:22 +0300

    feature: get products as observable

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { IProduct, PRODUCTS } from '../../components/product-list/product/product.model';
 
 const products: IProduct[] = PRODUCTS;
@@ -9,7 +10,7 @@ const products: IProduct[] = PRODUCTS;
 export class ProductService {
 	constructor() {}
 
-	public getProducts(): IProduct[] {
-		return products;
+	public getProducts(): Observable<IProduct[]> {
+		return of(products);
 	}
 }
