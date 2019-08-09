be8f0eaa47f78e3822321fcf1d2e43e309069eea
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-08-09 14:36:38 +0300

    fix: fix OrderByPipe for different field types

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -7,7 +7,12 @@ export class OrderByPipe implements PipeTransform {
 	transform(value: any[], field: string, asc: boolean = false): any {
 		return value.sort((a, b) => {
 			if (a.hasOwnProperty(field)) {
-				return asc ? a[field] - b[field] : b[field] - a[field];
+				if (typeof a[field] === 'number' && typeof b[field] === 'number') {
+					return asc ? a[field] - b[field] : b[field] - a[field];
+				}
+				if (typeof a[field] === 'string' && typeof b[field] === 'string') {
+					return asc ? a[field].localeCompare(b[field]) : b[field].localeCompare(a[field]);
+				}
 			}
 		});
 	}
