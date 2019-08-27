f8a812033087cd458fe446b9b4307c7933458b1f
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-08-27 12:43:14 +0300

    feature: add path-not-found component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HighlightDirective } from './directives/highlight.directive';
 import { FontDirective } from './directives/font.directive';
 import { OrderByPipe } from './pipes/order-by.pipe';
+import { PathNotFoundComponent } from './components/path-not-found/path-not-found.component';
 
 @NgModule({
-	declarations: [HighlightDirective, FontDirective, OrderByPipe],
+	declarations: [HighlightDirective, FontDirective, OrderByPipe, PathNotFoundComponent],
 	imports: [CommonModule],
 	providers: [OrderByPipe],
 	exports: [HighlightDirective, FontDirective],
