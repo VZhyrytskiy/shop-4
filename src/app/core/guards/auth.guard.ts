9d87d489419a676015b9383608fb0aaf2b5bd370
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-09-06 18:45:47 +0300

    feature: create auth service

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -3,14 +3,21 @@ import {
 	CanActivate,
 	ActivatedRouteSnapshot,
 	RouterStateSnapshot,
-	UrlTree
+	UrlTree,
+	Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+	constructor(
+		private authService: AuthService,
+		private router: Router,
+	) {}
+
 	canActivate(
 		next: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot,
@@ -20,6 +27,17 @@ export class AuthGuard implements CanActivate {
 		| boolean
 		| UrlTree {
 		console.log('CanActivate Guard is called');
-		return false; // true
+		const { url } = state; // destructurization
+		return this.checkLogin(url);
+	}
+
+	private checkLogin(url: string): boolean | UrlTree {
+		if (this.authService.isLoggedIn) { return true; }
+
+		// Store the attempted URL for redirecting
+		this.authService.redirectUrl = url;
+
+		// Navigate to the login page, return UrlTree
+		return this.router.parseUrl('/login');
 	}
 }
