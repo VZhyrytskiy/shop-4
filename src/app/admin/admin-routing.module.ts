ed6c2e4ca658106391a07f551aeb3a35d9a1e132
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-09-06 18:28:08 +0300

    feature: create canActivate guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent, ManageProductsComponent } from './components';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
 	{
 		path: 'admin',
 		component: AdminComponent,
+		canActivate: [AuthGuard],
 		children: [
 			{
 				path: '', // without component
