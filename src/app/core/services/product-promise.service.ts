97e25bada775d2a4e3aeb079802f4206d374c895
Nastassia.Tanyhina <nastassia.tanyhina@thomsonreuters.com>
2019-09-11 15:28:09 +0300

    feature: add product-promise service

---
+++ src/app/core/services/product-promise.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from '../../components/product-list/product/product.model';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ProductPromiseService {
+	private productsUrl = 'http://localhost:3000/products';
+
+	constructor(private http: HttpClient) {}
+
+	public getProducts(): Promise<IProduct[]> {
+		return this.http
+			.get(this.productsUrl)		// Observable
+			.toPromise()				// Observable to Promise
+			.then(response => response as IProduct[])		// Promise API
+			.catch(this.handleError);
+	}
+
+	private handleError(error: any): Promise<any> {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);		// error for the caller
+	}
+}
